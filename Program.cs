fc6f62218ed67306770be63c8c38442d5e49e899
fanmoe pouemo clovis <clovis.fanmoepouemo@monboreal.ca>
2024-10-24 23:51:11 -0800

    Ajout de la classe CarteCredit et instances dans Program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿internal class Program
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+   // private static void Main(string[] args)
+   // {
+   //     Console.WriteLine("Hello, World!");
+   // }
     // Création de la classe abstraite Paiement
     public abstract class Paiement
     {
@@ -18,5 +20,34 @@
 
         public abstract void EffectuerPaiement();
     }
+    // Création de la classe enfant CarteCredit
+    public class CarteCredit 
+    {
+        public int NumeroCarte { get; set; }
+       
+        public CarteCredit( int numeroCarte)
+           
+        {
+            NumeroCarte = numeroCarte;
+            
+        }
+
+        public  void EffectuerPaiement()
+        {
+            Console.WriteLine($"le numéro de la  carte de crédit est : {NumeroCarte} ");
+        }
+    }
+    static void Main (string[] args)
+    {
+        CarteCredit achat1 = new CarteCredit(10050);
+        CarteCredit achat2 = new CarteCredit(20075);
+        CarteCredit achat3 = new CarteCredit(5000);
+
+        achat1.EffectuerPaiement();
+        achat2.EffectuerPaiement();
+        achat3.EffectuerPaiement();
+    }
+
+
 
 }

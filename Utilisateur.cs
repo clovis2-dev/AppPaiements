1e28fae7f79b84df2cf86f64f2c8823d98e7b087
fanmoe pouemo clovis <clovis.fanmoepouemo@monboreal.ca>
2024-10-25 01:09:17 -0800

    Ajout de la classe Utilisateur

---
+++ Utilisateur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class Utilisateur
+    {
+        public string Name { get; set; }
+        public List<Paiement> Payments { get; set; }
+
+        public Utilisateur(string name)
+        {
+            Name = name;
+            Payments = new List<Paiement>();
+        }
+
+        public void AddPayment(Paiement payment)
+        {
+            Payments.Add(payment);
+        }
+
+        public void DisplayPayments()
+        {
+            Console.WriteLine($"Payments for user {Name}:");
+            foreach (var payment in Payments)
+            {
+                payment.EffectuerPaiement();
+            }
+        }
+    }
+
+}
